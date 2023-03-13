529d362a1eb96113c43e737d464aa92caf947b89
Bruno Filadelfo <Bruno.Filadelfo@Outlook.Com>
2023-03-13 01:36:27 -0300

    commits acrescentados

--- server.js
+++ server.js
@@ -4,7 +4,39 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function (reg, res){
-    res.render("pages/index");
+    const items = [
+        {
+            title: "D",
+            message: "Desenvolver aplicações/serviços de forma fácil"
+        },
+        {
+            title: "E",
+            message: "JS usa JavaScript para renderizar HTML"
+        },
+        {
+            title: "M",
+            message: "uito fácil de usar"
+        },
+        {
+            title: "A",
+            message: "morzinho"
+        },
+        {
+            title: "I",
+            message: "nstall ejs"
+        },
+        {
+            title: "S",
+            message: "intaxe simples"
+        },
+    ];
+
+    const subtitle = "Uma linguagem de modelagem para criação de página HTML utilizando JS"
+    
+    res.render("pages/index", {
+        qualitys: items,
+        subtitle: subtitle,
+    });
 })
 
 app.get("/sobre", function (req, res) {
